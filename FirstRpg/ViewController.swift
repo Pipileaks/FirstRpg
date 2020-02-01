f0bef56aa203c2ab869fb2a911ab333b3b673de0
UmutSever <severumut@yahoo.com>
2020-02-01 13:13:57 +0300

    Stepper Value Change and Count

--- FirstRpg/ViewController.swift
+++ FirstRpg/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var str_value: UILabel!
+    
+    @IBAction func str_value_changer(_ sender: UIStepper) {
+        str_value.text = String(sender.value)
+        var str_value_cal = sender.value
+        print(str_value_cal)
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +27,6 @@ class ViewController: UIViewController {
     }
 
 
+    
 }
 
