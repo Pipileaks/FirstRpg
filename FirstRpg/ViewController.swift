d5748bba576b6dcb16a71dbc94ba46fb0e61418f
UmutSever <severumut@yahoo.com>
2020-02-02 17:21:36 +0300

    Stepper Maximum If Tested

--- FirstRpg/ViewController.swift
+++ FirstRpg/ViewController.swift
@@ -10,13 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var str_value_cal : Int = 0
+    var dex_value_cal : Int = 0
+    var str_max_cal : Int = 0
+    var dex_max_cal : Int = 0
+    var maks = 6
+    
     
     @IBOutlet weak var str_value: UILabel!
+    @IBOutlet weak var dex_value: UILabel!
     
     @IBAction func str_value_changer(_ sender: UIStepper) {
         str_value.text = String(sender.value)
-        var str_value_cal = sender.value
+        str_value_cal = Int(sender.value)
         print(str_value_cal)
+        str_max_cal = Int(sender.maximumValue)
+        
+      
+        let calculation = str_value_cal + dex_value_cal
+        
+        sender.maximumValue = Double(calculation >= maks ? str_value_cal: maks)
+        dex_max_cal = calculation >= maks ? dex_value_cal : maks
+    
+        
+    }
+
+    @IBAction func dex_value_changer(_ sender: UIStepper) {
+        dex_value.text = String(sender.value)
+        dex_value_cal = Int(sender.value)
+        dex_max_cal = Int(sender.maximumValue)
+        print(dex_value_cal)
+        
+         let calculation = str_value_cal + dex_value_cal
+        
+        sender.maximumValue = Double(calculation >= maks ? dex_value_cal: maks)
+        str_max_cal = calculation >= maks ? str_value_cal : maks
+        
+        
+        
+
     }
     
     
@@ -25,7 +57,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+   
+ 
+    
 
     
 }
