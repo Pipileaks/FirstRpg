21ff0c6c6898f13c3c3ca27c82da2b9612272555
UmutSever <severumut@yahoo.com>
2020-02-09 18:27:01 +0300

    Update Dice Roller and Roll Button

--- FirstRpg/Controllers/MainViewController.swift
+++ FirstRpg/Controllers/MainViewController.swift
@@ -17,6 +17,15 @@ class MainViewController: UIViewController {
     var characterAllocatedInt: Int?
     var characterAllocatedCha: Int?
     
+    @IBOutlet weak var fButtonOutlet: UIButton!
+    @IBOutlet weak var sButtonOutlet: UIButton!
+    @IBOutlet weak var tButtonOutlet: UIButton!
+    @IBOutlet weak var frButtonOutlet: UIButton!
+    @IBOutlet weak var ffthButtonOutlet: UIButton!
+    
+
+  
+    
     var storyBankDic : [String : String] =
                     ["prologue" : "Lanet olsun...",
                     "storyOne" : "Burada storyOne texti olcak",
@@ -42,7 +51,7 @@ class MainViewController: UIViewController {
                     "storyFiveRight"    : "Burada storyFiveRight texti olcak"
                     ]
     
-    var storySelectionStatsBankDic : [String : Int] = ["int" : 2, "cha" : 2, "wis" : 1]
+    var storySelectionStatsBankDic : [String : Int] = ["str" : 2, "dex" : 2, "wis" : 1]
  
                    
     
@@ -54,7 +63,7 @@ class MainViewController: UIViewController {
 
         allocatedSkillLog()
         makeAllocatedStatsDic()
-        diceRoller()
+        
     }
     
     @IBAction func backToMain(_ sender: UIButton) {
@@ -62,20 +71,71 @@ class MainViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func mainRollButton(_ sender: UIButton) {
+
+        button1()
+        button2()
+        button3()
+        button4()
+        button5()
+    }
+    
+    
+    
+    
+               
+            
+    //        @IBOutlet weak var fButtonOutlet: UIButton!
+    //           @IBOutlet weak var sButtonOutlet: UIButton!
+    //           @IBOutlet weak var tButtonOutlet: UIButton!
+    //           @IBOutlet weak var frButtonOutlet: UIButton!
+    //           @IBOutlet weak var ffthButtonOutlet: UIButton!
+            
+             
+    func button1 () {
+        fButtonOutlet.setImage(UIImage(named: diceRoller()), for: .normal)
+        
+      
+        
+    }
+    func button2 () {
+
+        sButtonOutlet.setImage(UIImage(named: diceRoller()), for: .normal)
+    }
+    func button3 () {
+
+        tButtonOutlet.setImage(UIImage(named: diceRoller()), for: .normal)
+    }
+    func button4 () {
+
+        frButtonOutlet.setImage(UIImage(named: diceRoller()), for: .normal)
+    }
+    func button5 () {
+
+        ffthButtonOutlet.setImage(UIImage(named: diceRoller()), for: .normal)
+    }
+   
+    
+    
+    
+    
+    
+    
+    
     func allocatedSkillLog() {
         
-        print(characterAllocatedStr!)
-        print(characterAllocatedDex!)
-        print(characterAllocatedCon!)
-        print(characterAllocatedWis!)
-        print(characterAllocatedInt!)
-        print(characterAllocatedCha!)
+//        print(characterAllocatedStr!)
+//        print(characterAllocatedDex!)
+//        print(characterAllocatedCon!)
+//        print(characterAllocatedWis!)
+//        print(characterAllocatedInt!)
+//        print(characterAllocatedCha!)
   
     }
     
     // This func makes a dict from char allocated stats
     func makeAllocatedStatsDic () {
-        var allocatedStatsDic : [String: Int] = ["str": characterAllocatedStr!,
+        let allocatedStatsDic : [String: Int] = ["str": characterAllocatedStr!,
                                                  "dex": characterAllocatedDex!,
                                                  "con": characterAllocatedCon!,
                                                  "wis": characterAllocatedWis!,
@@ -87,7 +147,7 @@ class MainViewController: UIViewController {
     
     // This func makes a dict from char allocated stats and make dice percentages
     func makeAllocatedStatsPercDic () {
-        var allocatedStatsPercDic : [String: Int] = ["str": characterAllocatedStr!*5,
+        let allocatedStatsPercDic : [String: Int] = ["str": characterAllocatedStr!*5,
                                                  "dex": characterAllocatedDex!*5,
                                                  "con": characterAllocatedCon!*5,
                                                  "wis": characterAllocatedWis!*5,
@@ -103,13 +163,13 @@ class MainViewController: UIViewController {
     
    // This func rolls dice for player
     func diceRoller () -> String {
-        var allocatedStatsDiceDic : [String: Int] =
-            ["str": characterAllocatedStr!*5,
-             "dex": characterAllocatedDex!*5,
-             "con": characterAllocatedCon!*5,
-             "wis": characterAllocatedWis!*5,
-             "int": characterAllocatedInt!*5,
-             "cha": characterAllocatedCha!*5]
+        let allocatedStatsDiceDic : [String: Int] =
+            ["str": characterAllocatedStr!+1*5,
+             "dex": characterAllocatedDex!+1*5,
+             "con": characterAllocatedCon!+1*5,
+             "wis": characterAllocatedWis!+1*5,
+             "int": characterAllocatedInt!+1*5,
+             "cha": characterAllocatedCha!+1*5]
         
         var pipi = [String: Int]()
         var sum = 0
@@ -136,8 +196,8 @@ class MainViewController: UIViewController {
                 
                 
                 sum += b
-                print("Skill Başına Toplam Değer:", sum)
-                print(key)
+                
+               
                 ata.append(b)
                 
                 pipi.updateValue(sum, forKey: key)
@@ -145,25 +205,25 @@ class MainViewController: UIViewController {
             }
             
         }
-        var umut = Int.random(in: 1...sum)
-        print("Random Number:", umut)
-        print("Toplam Değer:", sum)
-        var sortedPipi = pipi.sorted{ $1.1 > $0.1 }
-        print("Sortlu:", sortedPipi)
+        let umut = Int.random(in: 1...sum)
+        
+        
+        let sortedPipi = pipi.sorted{ $1.1 > $0.1 }
+       
         
         if umut <= sortedPipi[0].value {
-            print(sortedPipi[0].key)
+           
             alp = sortedPipi[0].key
         } else if umut <= sortedPipi[1].value {
-            print(sortedPipi[1].key)
+           
             alp = sortedPipi[1].key
         } else {
-            print(sortedPipi[2].key)
+            
             alp = sortedPipi[2].key
         }
-        print("Alp Değer:", alp)
+       
         lastVersion = alp + ".png"
-        print("Last Version:", lastVersion)
+        
          print("Last Version:", lastVersion)
         return lastVersion
     }
